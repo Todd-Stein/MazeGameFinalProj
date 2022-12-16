6aecd779315e0f34d5750bb63244ab2157dcc3e1
Todd-Stein <tstein9@kent.edu>
2022-12-16 12:15:53 -0500

    just a few more touches

--- MazeGameFinalProj/ViewController.swift
+++ MazeGameFinalProj/ViewController.swift
@@ -41,68 +41,92 @@ enum direction {
 class ViewController: UIViewController {
     
     
-    var playerPos:pair<Int, Int> = pair(first: 0, second: 0)
+    @IBOutlet weak var outerBoundsTop: UIView!
     
+    @IBOutlet weak var outerBoundsLeft: UIView!
+    
+    
+    @IBOutlet weak var outerBoundsBottom: UIView!
+    
+    @IBOutlet weak var outerBoundsRight: UIView!
+    
+    
+    var playerPos:pair<Int, Int> = pair(first: 0, second: 0)
     var playerRect:CGRect = (CGRect(x: 0, y: 0, width: 1, height: 1))
     var playerCell:UIView = UIView()
+    var playerColor:UIColor = .blue
     
+    
+    var goalRect:CGRect = CGRect(x: 0, y:0, width: 1, height: 1)
+    var goalCell:UIView = UIView()
+    var goalColor:UIColor = .green
+    
+    var boundsColor:UIColor = .red
+    
+    
+    @IBAction func swipeRight(_ sender: Any) {
+        MoveRight()
+    }
+    @IBAction func swipeLeft(_ sender: Any) {
+            MoveLeft()
+    }
+    @IBAction func swipeDown(_ sender: Any) {
+            MoveDown()
+    }
+    @IBAction func swipeUp(_ sender: Any) {
+            MoveUp()
+    }
     
     @IBOutlet weak var displayBounds: UIView!
     
-    @IBAction func upPressed(_ sender: Any) {
-        if(playerPos.second-1>0) {
-            if(maze[playerPos.first][playerPos.second-1].dirs.contains(direction.north)) {
-                playerPos.second = playerPos.second-1
-                playerCell.removeFromSuperview()
-                var xPos:Int = playerPos.first
-                var yPos:Int = playerPos.second
-                xPos = xPos*(southWallWidth ?? 0)
-                yPos = yPos*(eastWallHeight ?? 0)
-                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
-                playerCell = UIView(frame: playerRect)
-                playerCell.backgroundColor = .blue
-                displayBounds.addSubview(playerCell)
-            }
-        }
-    }
-    @IBAction func leftPressed(_ sender: Any) {
-        if(playerPos.second+1<mazeHeight) {
-            if(maze[playerPos.first][playerPos.second+1].dirs.contains(direction.south)) {
-                playerPos.second = playerPos.second+1
-                playerPos.second = playerPos.second-1
-                playerCell.removeFromSuperview()
-                var xPos:Int = playerPos.first
-                var yPos:Int = playerPos.second
-                xPos = xPos*(southWallWidth ?? 0)
-                yPos = yPos*(eastWallHeight ?? 0)
-                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
-                playerCell = UIView(frame: playerRect)
-                playerCell.backgroundColor = .blue
-                displayBounds.addSubview(playerCell)
-            }
-        }
-    }
-    @IBAction func downPressed(_ sender: Any) {
-        if(playerPos.first-1>0) {
-            if(maze[playerPos.first-1][playerPos.second].dirs.contains(direction.west)) {
-                playerPos.first = playerPos.first-1
-                playerPos.second = playerPos.second-1
-                playerCell.removeFromSuperview()
-                var xPos:Int = playerPos.first
-                var yPos:Int = playerPos.second
-                xPos = xPos*(southWallWidth ?? 0)
-                yPos = yPos*(eastWallHeight ?? 0)
-                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
-                playerCell = UIView(frame: playerRect)
-                playerCell.backgroundColor = .blue
-                displayBounds.addSubview(playerCell)
-            }
-        }
-    }
-    @IBAction func rightPressed(_ sender: Any) {
+    func MoveRight() {
         if(playerPos.first+1<mazeWidth) {
-            if(maze[playerPos.first+1][playerPos.second].dirs.contains(direction.east)) {
+            print("moveRight1")
+            if(maze[playerPos.first][playerPos.second].dirs.contains(direction.east)) {
+                print("moveRight2")
                 playerPos.first = playerPos.first+1
+                playerCell.removeFromSuperview()
+                var xPos:Int = playerPos.first
+                var yPos:Int = playerPos.second
+                xPos = xPos*(southWallWidth ?? 0)
+                yPos = yPos*(eastWallHeight ?? 0)
+                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
+                playerCell = UIView(frame: playerRect)
+                playerCell.backgroundColor = .blue
+                displayBounds.addSubview(playerCell)
+                displayBounds.sendSubviewToBack(playerCell)
+                displayBounds.sendSubviewToBack(goalCell)
+            }
+        }
+    }
+    func MoveLeft() {
+        print(playerPos.first)
+        if(playerPos.first-1>=0) {
+            print("moveLeft1")
+            if(maze[playerPos.first][playerPos.second].dirs.contains(direction.west))
+            {
+                print("moveLeft2")
+                playerPos.first = playerPos.first-1
+                playerCell.removeFromSuperview()
+                var xPos:Int = playerPos.first
+                var yPos:Int = playerPos.second
+                xPos = xPos*(southWallWidth ?? 0)
+                yPos = yPos*(eastWallHeight ?? 0)
+                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
+                playerCell = UIView(frame: playerRect)
+                playerCell.backgroundColor = .blue
+                displayBounds.addSubview(playerCell)
+                displayBounds.sendSubviewToBack(playerCell)
+                displayBounds.sendSubviewToBack(goalCell)
+            }
+        }
+    }
+    
+    func MoveUp() {
+        if(playerPos.second-1>0) {
+            print("moveUp1")
+            if(maze[playerPos.first][playerPos.second].dirs.contains(direction.north)) {
+                print("moveUp2")
                 playerPos.second = playerPos.second-1
                 playerCell.removeFromSuperview()
                 var xPos:Int = playerPos.first
@@ -113,9 +137,35 @@ class ViewController: UIViewController {
                 playerCell = UIView(frame: playerRect)
                 playerCell.backgroundColor = .blue
                 displayBounds.addSubview(playerCell)
+                displayBounds.sendSubviewToBack(playerCell)
+                displayBounds.sendSubviewToBack(goalCell)
             }
         }
     }
+    
+    
+    func MoveDown() {
+        if(playerPos.second+1<mazeHeight) {
+            print("moveDown1")
+            if(maze[playerPos.first][playerPos.second].dirs.contains(direction.south)) {
+                print("moveDown2")
+                playerPos.second = playerPos.second+1
+                playerCell.removeFromSuperview()
+                var xPos:Int = playerPos.first
+                var yPos:Int = playerPos.second
+                xPos = xPos*(southWallWidth ?? 0)
+                yPos = yPos*(eastWallHeight ?? 0)
+                playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
+                playerCell = UIView(frame: playerRect)
+                playerCell.backgroundColor = .blue
+                displayBounds.addSubview(playerCell)
+                displayBounds.sendSubviewToBack(playerCell)
+                displayBounds.sendSubviewToBack(goalCell)
+            }
+        }
+    }
+    
+    
     var mazeBounds:[UIView] = []
     var mazeWidth:Int = 20
     var mazeHeight:Int = 10
@@ -237,6 +287,58 @@ class ViewController: UIViewController {
             }
         }
     }
+    func drawMaze() {
+        var canGoSouth:Bool = false
+        var canGoEast:Bool = false
+        for i in 0...mazeWidth-1 {
+            for j in 0...mazeHeight-1 {
+                canGoEast = maze[i][j].dirs.contains(direction.east)
+                canGoSouth = maze[i][j].dirs.contains(direction.south)
+                if(i<mazeWidth-1) {
+                    if (!canGoEast) {
+                        let bound:CGRect = CGRect(x: ((southWallWidth ?? 0)*(i+1)), y: ((eastWallHeight ?? 0)*j), width: eastWallWidth ?? 0, height: eastWallHeight ?? 0)
+                        let boundView:UIView = UIView(frame: bound)
+                        boundView.backgroundColor = .red
+                        mazeBounds.append(boundView)
+                        displayBounds.addSubview(boundView)
+                    }
+                }
+                if(j<mazeHeight-1) {
+                    if(!canGoSouth) {
+                        let bound:CGRect = CGRect(x: ((southWallWidth ?? 0)*(i)), y: ((eastWallHeight ?? 0)*(j+1)), width: southWallWidth ?? 0, height: southWallHeight ?? 0)
+                        let boundView:UIView = UIView(frame: bound)
+                        boundView.backgroundColor = boundsColor
+                        mazeBounds.append(boundView)
+                        displayBounds.addSubview(boundView)
+                    }
+                }
+                canGoEast = false
+                canGoSouth = false
+            }
+        }
+        var xPos:Int = playerPos.first
+        var yPos:Int = playerPos.second
+        xPos = xPos*(southWallWidth ?? 0)
+        yPos = yPos*(eastWallHeight ?? 0)
+        playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
+        playerCell = UIView(frame: playerRect)
+        playerCell.backgroundColor = playerColor
+        displayBounds.addSubview(playerCell)
+        displayBounds.sendSubviewToBack(playerCell)
+        outerBoundsTop.backgroundColor = boundsColor
+        outerBoundsLeft.backgroundColor = boundsColor
+        outerBoundsRight.backgroundColor = boundsColor
+        outerBoundsBottom.backgroundColor = boundsColor
+        xPos = southWallWidth ?? 0
+        xPos = xPos * mazeWidth
+        yPos = eastWallHeight ?? 0
+        yPos = yPos * mazeHeight
+        goalRect = CGRect(x: xPos, y: yPos, width: southWallWidth ?? 0, height: eastWallHeight ?? 0)
+        goalCell = UIView(frame: goalRect)
+        goalCell.backgroundColor = goalColor
+        displayBounds.sendSubviewToBack(goalCell)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -254,65 +356,7 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        
-        var canGoSouth:Bool = false
-        var canGoEast:Bool = false
-        for i in 0...mazeWidth-1 {
-            for j in 0...mazeHeight-1 {
-                canGoEast = maze[i][j].dirs.contains(direction.east)
-                canGoSouth = maze[i][j].dirs.contains(direction.south)
-                if(i<mazeWidth-1) {
-                    if (!canGoEast) {
-                        var x:CGFloat = CGFloat(((southWallWidth ?? 0)*(i+1)))
-                        if(j<mazeHeight-1) {
-                            x = x-CGFloat(((southWallWidth ?? 0)/2))
-                        }
-                        //if(i<mazeWidth/2) {
-                        //    x = x+1
-                        //}
-                        //else if(i>mazeWidth/2) {
-                        //    x = x-1
-                        //}
-                        let bound:CGRect = CGRect(x: Int(x), y: ((eastWallHeight ?? 0)*j), width: eastWallWidth ?? 0, height: eastWallHeight ?? 0)
-                        let boundView:UIView = UIView(frame: bound)
-                        boundView.backgroundColor = .red
-                        mazeBounds.append(boundView)
-                        displayBounds.addSubview(boundView)
-                    }
-                }
-                if(j<mazeHeight-1) {
-                    if(!canGoSouth) {
-                        var y:CGFloat = CGFloat(((eastWallWidth ?? 0)*(j+1)))
-                        if(i<mazeWidth-1) {
-                            y = y-CGFloat(((eastWallWidth ?? 0)/2))
-                        }
-                        //if(j<mazeHeight/2) {
-                        //    y = y+5
-                        //}
-                        //else if(j>mazeHeight/2) {
-                        //    y = y-5
-                        //}
-                        let bound:CGRect = CGRect(x: ((southWallWidth ?? 0)*(i)), y: ((eastWallHeight ?? 0)*(j+1)), width: southWallWidth ?? 0, height: southWallHeight ?? 0)
-                        let boundView:UIView = UIView(frame: bound)
-                        boundView.backgroundColor = .red
-                        mazeBounds.append(boundView)
-                        displayBounds.addSubview(boundView)
-                    }
-                }
-                canGoEast = false
-                canGoSouth = false
-            }
-        }
-        var xPos:Int = playerPos.first
-        var yPos:Int = playerPos.second
-        xPos = xPos*(southWallWidth ?? 0)
-        yPos = yPos*(eastWallHeight ?? 0)
-        playerRect = (CGRect(x: xPos, y: yPos, width: (southWallWidth ?? 0), height: (eastWallHeight ?? 0)))
-        playerCell = UIView(frame: playerRect)
-        playerCell.backgroundColor = .blue
-        displayBounds.addSubview(playerCell)
-        
+        drawMaze()
     }
 
 }
